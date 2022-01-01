08f4b424893bebdcf1d78c76f0f463d7c9f4b0ed
vanduy24072002 <85008749+vanduy24072002@users.noreply.github.com>
2022-01-01 15:57:50 +0700

    11

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-        .thu-nghiem-zoom img{display:block;transition: all .3s ease;}
-        .thu-nghiem-zoom img:hover{transform: scale(1.1);}
-        .thu-nghiem-zoom p{display:block;overflow: hidden;}
+        .menu_left img{display:block;transition: all .3s ease;}
+        .menu_left img:hover{transform: scale(1.05);}
+        .menu_left p{display:block;overflow: hidden;}
     </style>
     </style>
   
